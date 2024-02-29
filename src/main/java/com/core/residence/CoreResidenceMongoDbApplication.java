9a32f944386be8ae9e6b946bc9b4fb72cd39ca85
xinove <jesusmagana1984@yahoo.es>
2024-02-29 01:50:33 +0100

    Primera versión
    Primera versión

---
+++ src/main/java/com/core/residence/CoreResidenceMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.core.residence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreResidenceMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreResidenceMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/residence/CoreResidenceMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.residence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreResidenceMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
